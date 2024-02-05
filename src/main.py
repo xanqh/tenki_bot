2a9c50b0330c2f77e98e41f7b9803b100394644d
xanqh <jehgeg1234@gmail.com>
2024-02-05 19:26:56 +0900

    deploy using heroku

--- src/main.py
+++ src/main.py
@@ -1,20 +1,87 @@
 import json
-from linebot import LineBotApi
-from linebot.models import TextSendMessage
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageMessage, ImageSendMessage, FollowEvent, UnfollowEvent
 import requests
 from bs4 import BeautifulSoup
 import re
+from io import BytesIO
+import psycopg2
+from flask import Flask, request, abort
+import os
 
 # 設定情報読み込み
-with open("settings.json", encoding="utf-8") as f:
+with open("setting/settings.json", encoding="utf-8") as f:
     res = json.load(f)
 
 # チャネルアクセストークン
 CH_TOKEN = res["CH_TOKEN"]
 # userID
 USER_ID = res["USER_ID"]
+# チャネルシークレット
+CH_SECRET = res["CH_SECRET"]
 # 天気予報URL
 URL = "https://tenki.jp/forecast/5/26/5110/23100/"
+# 後ほどHerokuでPostgreSQLデータベースURLを取得
+DATABASE_URL = res["DB_URL"]
+# 後ほど作成するHerokuアプリ名
+HEROKU_APP_NAME = res["HEROKU_NAME"]
+
+app = Flask(__name__)
+Heroku = "https://{}.herokuapp.com/".format(HEROKU_APP_NAME)
+
+line_bot_api = LineBotApi(CH_TOKEN)
+handler = WebhookHandler(CH_SECRET)
+
+# テスト用
+@app.route("/")
+def hello_world():
+    return "hello world!"
+
+# アプリにPOSTがあったときの処理
+@app.route("/callback", methods=["POST"])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers["X-Line-Signature"]
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return "OK"
+
+# データベース接続
+def get_connection():
+    return psycopg2.connect(DATABASE_URL, sslmode="require")
+
+# botがフォローされたときの処理
+@handler.add(FollowEvent)
+def handle_follow(event):
+    profile = line_bot_api.get_profile(event.source.user_id)
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            conn.autocommit = True
+            cur.execute('CREATE TABLE IF NOT EXISTS users(user_id TEXT)')
+            cur.execute('INSERT INTO users (user_id) VALUES (%s)', [profile.user_id])
+            print('userIdの挿入OK!!')
+            cur.execute('SELECT * FROM users')
+            db = cur.fetchall()
+    print("< データベース一覧 >")
+    for db_check in db:
+        print(db_check)
+
+
+# botがアンフォロー(ブロック)されたときの処理
+@handler.add(UnfollowEvent)
+def handle_unfollow(event):
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            conn.autocommit = True
+            cur.execute('DELETE FROM users WHERE user_id = %s', [event.source.user_id])
+    print("userIdの削除OK!!")
 
 def get_page_info():
     """ 読み込みページ情報取得(URLにリクエストしてBeautifulSoupで整形) """
@@ -106,25 +173,29 @@ def create_msg(weather_title, weather_list):
 
     return bot_msg
 
-def main():
-    """ LINE BOTメイン処理 """
+@handler.add(MessageEvent,message=TextMessage)
+def handle_message(event):
+    #入力された文字を取得
+    text_in = event.message.text
 
-    # 天気予報ページ情報取得
-    soup = get_page_info()
+    if "天気" in text_in:   #scw.pyのgetw関数を呼び出している
+        # 天気予報ページ情報取得
+        soup = get_page_info()
 
-    # ページタイトル
-    page_title = soup.title.text
-    m = re.search(".*天気", page_title)
-    weather_title = m.group(0) # 名古屋市の今日明日の天気
+        # ページタイトル
+        page_title = soup.title.text
+        m = re.search(".*天気", page_title)
+        weather_title = m.group(0) # 名古屋市の今日明日の天気
 
-    # 今日明日の天気予報情報
-    weather_list = get_weather_info(soup)
+        # 今日明日の天気予報情報
+        weather_list = get_weather_info(soup)
 
-    # LINE BOTメッセージ
-    msg = create_msg(weather_title, weather_list)
-    messages = TextSendMessage(text=msg)
-    line_bot_api = LineBotApi(CH_TOKEN)
-    line_bot_api.push_message(USER_ID, messages=messages)
+        # LINE BOTメッセージ
+        msg = create_msg(weather_title, weather_list)
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=msg))
+    else:   #「今日」「明日」以外の文字はオウム返しする
+     line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
 
-if __name__ == "__main__":
-    main()
+if __name__=="__main__":
+    port=int(os.getenv("PORT",5000))
+    app.run(host="0.0.0.0",port=port)
